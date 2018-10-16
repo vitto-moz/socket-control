7fd895f60f0111bd6ef71b4df8df023872608307
v.mozharovsky <v.mozharovsky@gmail.com>
2018-10-16 20:05:43 +0300

    static added

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@
 import express from 'express';
 import http from 'http';
 import SocketIO from 'socket.io';
+import path from 'path'
 
+const publicPath = path.join(__dirname, '../public') 
 let app = express();
 let server = http.Server(app);
 let io = new SocketIO(server);
 let port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath))
 
 io.on('connection', (socket) => {
   console.log("Connection");
